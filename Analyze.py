9e0a51858011d283443bc27cf5155f1b1e7519f3
AndrewZhang419 <63554584+AndrewZhang419@users.noreply.github.com>
2025-07-09 12:38:02 -0400

    working version

--- Analyze.py
+++ Analyze.py
@@ -13,217 +13,151 @@
 #     name: python3
 # ---
 
-# +
+# Configuration parameters
+openslidelevel = 0            # level from openslide to read
+tilesize = 10000              # size of the tile to load from openslide
+patchsize = 32                # patch size needed by our DL model
 
-openslidelevel = 0  # level from openslide to read
-tilesize = 10000  # size of the tile to load from openslide
-patchsize = 32  # patch size needed by our DL model
+minhits = 100                 # minimum objects per tile to process
+batchsize = 1024              # number of patches per batch for inference
+nclasses = 2                  # number of output classes for the model
+classnames = ["Other", "Lymphocyte"]  # class names for QuPath visualization
+colors = [-377282, -9408287]  # corresponding RGB colors for those classes
 
-minhits = 100  # the minimum number of objects needed to be present within a tile for the tile to be computed on
-batchsize = 1024  # how many patches we want to send to the GPU at a single time
-nclasses = 2  # number of output classes our model is providing
-classnames = ["Other", "Lymphocyte"]  # the names of those classes which will appear in QuPath later on
-colors = [-377282,
-          -9408287]  # their associated color, see selection of different color values at the bottom of the file
+mask_patches = False          # blackout background around objects if trained that way
 
-mask_patches = False  # if we woud like to blackout the space around the object of interest, this is determined by how the model was trained
+# File paths
+json_fname = r'MTA38GPC.json'                  # input GeoJSON file
+json_annotated_fname = r'1L1_nuclei_reg_anno.json'  # output annotated GeoJSON file
+model_fname = "lymph_model.pth"              # path to DL model
+wsi_fname = "MTA3-8_GPC3.svs"                 # whole-slide image file
 
-json_fname = r'MTA38GPC.json'  # input geojson file
-json_annotated_fname = r'1L1_nuclei_reg_anno.json'  # target output geojson file
-model_fname = "lymph_model.pth"  # DL model to use
-wsi_fname = "MTA3-8_GPC3.svs"  # whole slide image fname to load cells from which coincide with the json file
-
-# -
-
+# --- Imports ---
 import os
-import openslide
-
-print("Looking for file:", os.path.abspath(wsi_fname))
-print("File exists?", os.path.exists(wsi_fname))
-
-print("Current directory:", os.getcwd())
-
-os.environ['PATH'] = '~/Documents/GitHub/QuPath' + ';' + os.environ[
-    'PATH']  # can either specify openslide bin path in PATH, or add it dynamically
-
-from tqdm.autonotebook import tqdm
-from math import ceil
-import matplotlib.pyplot as plt
-
-import geojson
-from shapely.geometry import shape
-from shapely.strtree import STRtree
-from shapely.geometry import Point
-from shapely.geometry import Polygon
-
-# +
-import torch
-from torch import nn
-from torchsummary import summary
+import gzip
 import numpy as np
 import cv2
-import gzip
+import openslide
+from math import ceil
+import geojson
+from shapely.geometry import shape, Polygon
+from shapely.strtree import STRtree
+import torch
 
-device = torch.device('cuda')
+# --- Setup ---
+print("Looking for file:", os.path.abspath(wsi_fname))
+print("Exists?", os.path.exists(wsi_fname))
+print("Current directory:", os.getcwd())
 
+os.environ['PATH'] = os.path.expanduser('~/Documents/GitHub/QuPath') + os.pathsep + os.environ.get('PATH', '')
 
-def divide_batch(l, n):
-    for i in range(0, l.shape[0], n):
-        yield l[i:i + n, ::]
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Using device: {device}")
 
-    # --- Load your model here
+def divide_batch(arr, n):
+    for i in range(0, arr.shape[0], n):
+        yield arr[i:i + n]
 
-
-# model = LoadYourModelHere().to(device)
-# checkpoint = torch.load(model_fname, map_location=lambda storage, loc: storage)  # load checkpoint to CPU and then put to device https://discuss.pytorch.org/t/saving-and-loading-torch-models-on-2-machines-with-different-number-of-gpu-devices/6666
-# model.load_state_dict(checkpoint["model_dict"])
+# --- Load Model (stub) ---
+# model = MyModel().to(device)
+# checkpoint = torch.load(model_fname, map_location=device)
+# model.load_state_dict(checkpoint['model_dict'])
 # model.eval()
-# summary(model, (3, 32, 32))
 
-# +
-
-if json_fname.endswith(".gz"):
+# --- Load GeoJSON ---
+if json_fname.endswith('.gz'):
     with gzip.GzipFile(json_fname, 'r') as f:
-        allobjects = geojson.loads(f.read(), encoding='ascii')
+        allobjects = geojson.loads(f.read().decode('ascii'))
 else:
     with open(json_fname) as f:
         allobjects = geojson.load(f)
+print(f"Loaded {len(allobjects)} objects from {json_fname}")
 
-print("done loading")
-# -
-
-allshapes = [shape(obj["nucleusGeometry"] if "nucleusGeometry" in obj.keys() else obj["geometry"]) for obj in
-             allobjects]
+# Build shapes and centroids
+allshapes = [shape(obj.get('nucleusGeometry', obj.get('geometry'))) for obj in allobjects]
 allcenters = [s.centroid for s in allshapes]
-print("done converting")
-
-
-###for i in range(len(allshapes)):
-    ###allcenters[i] = i
-
 searchtree = STRtree(allcenters)
-print("done building tree")
 
-# +
+# Open WSI
 osh = openslide.OpenSlide(wsi_fname)
-nrow, ncol = osh.level_dimensions[0]
-nrow = ceil(nrow / tilesize)
-ncol = ceil(ncol / tilesize)
-
+level_dims = osh.level_dimensions[openslidelevel]
 scalefactor = int(osh.level_downsamples[openslidelevel])
-paddingsize = patchsize // 2 * scalefactor
+paddingsize = (patchsize // 2) * scalefactor
+int_coords = lambda coords: np.array(coords).round().astype(np.int32)
 
-int_coords = lambda x: np.array(x).round().astype(np.int32)
-
-# +
-for y in tqdm(range(0, osh.level_dimensions[0][1], round(tilesize * scalefactor)), desc="outer", leave=False):
-    for x in tqdm(range(0, osh.level_dimensions[0][0], round(tilesize * scalefactor)), desc=f"inner {y}", leave=False):
-
-        tilepoly = Polygon([[x, y], [x + tilesize * scalefactor, y],
-                            [x + tilesize * scalefactor, y + tilesize * scalefactor],
-                            [x, y + tilesize * scalefactor]])
-        hits = searchtree.query(tilepoly)
-
-        if len(hits) < minhits:
+# --- Tile iteration (no progress bars) ---
+for y in range(0, level_dims[1], round(tilesize * scalefactor)):
+    for x in range(0, level_dims[0], round(tilesize * scalefactor)):
+        tilepoly = Polygon([
+            (x, y),
+            (x + tilesize * scalefactor, y),
+            (x + tilesize * scalefactor, y + tilesize * scalefactor),
+            (x, y + tilesize * scalefactor)
+        ])
+        hits_idx = [int(h) for h in searchtree.query(tilepoly)]
+        if len(hits_idx) < minhits:
             continue
 
-        tile = np.asarray(osh.read_region((x - paddingsize, y - paddingsize), openslidelevel,
-                                          (tilesize + 2 * paddingsize, tilesize + 2 * paddingsize)))[:, :,
-               0:3]  # trim alpha
+        tile = np.asarray(
+            osh.read_region(
+                (x - paddingsize, y - paddingsize),
+                openslidelevel,
+                (tilesize + 2 * paddingsize, tilesize + 2 * paddingsize)
+            )
+        )[:, :, :3]
 
         if mask_patches:
-            mask = np.zeros((tile.shape[0:2]), dtype=tile.dtype)
-            exteriors = [int_coords(allshapes[hit.id].boundary.coords) for hit in hits]
-            exteriors_shifted = [(ext - np.asarray([(x - paddingsize), (y - paddingsize)])) // scalefactor for ext in
-                                 exteriors]
+            mask = np.zeros(tile.shape[:2], dtype=tile.dtype)
+            exteriors = [int_coords(allshapes[i].boundary.coords) for i in hits_idx]
+            exteriors_shifted = [
+                (ext - np.array([x - paddingsize, y - paddingsize])) // scalefactor
+                for ext in exteriors
+            ]
             cv2.fillPoly(mask, exteriors_shifted, 1)
 
-        arr_out = np.zeros((len(hits), patchsize, patchsize, 3))
-        id_out = np.zeros((len(hits), 1))
+        arr_out = np.zeros((len(hits_idx), patchsize, patchsize, 3), dtype=tile.dtype)
+        id_out = np.zeros((len(hits_idx), 1), dtype=int)
 
-        # ---- get patches from hits within this tile and stick them (and their ids) into matricies
-        for hit, arr, id in zip(hits, arr_out, id_out):
-            px, py = hit.coords[:][
-                0]  # this way is faster than using hit.x and hit.y, likely because of call stack overhead
-            c = int((px - x + paddingsize) // scalefactor)
-            r = int((py - y + paddingsize) // scalefactor)
-            patch = tile[r - patchsize // 2:r + patchsize // 2, c - patchsize // 2:c + patchsize // 2, :]
-
+        for i, idx in enumerate(hits_idx):
+            cx, cy = allcenters[idx].x, allcenters[idx].y
+            col = int((cx - x + paddingsize) // scalefactor)
+            row = int((cy - y + paddingsize) // scalefactor)
+            patch = tile[
+                row - patchsize//2:row + patchsize//2,
+                col - patchsize//2:col + patchsize//2,
+                :
+            ]
             if mask_patches:
-                maskpatch = mask[r - patchsize // 2:r + patchsize // 2, c - patchsize // 2:c + patchsize // 2]
-                patch = np.multiply(patch, maskpatch[:, :, None])
+                mp = mask[row - patchsize//2:row + patchsize//2,
+                          col - patchsize//2:col + patchsize//2]
+                patch = patch * mp[:, :, None]
+            arr_out[i] = patch
+            id_out[i, 0] = idx
 
-            arr[:] = patch
-
-            id[:] = hit.id
-
-        # ---- process batch
-
+        # Classify patches in batches (no outer progress bars)
+        batches = list(divide_batch(arr_out, batchsize))
         classids = []
-        for batch_arr in tqdm(divide_batch(arr_out, batchsize), leave=False):
-            batch_arr_gpu = torch.from_numpy(batch_arr.transpose(0, 3, 1, 2)).type('torch.FloatTensor').to(device) / 255
+        if batches:
+            for batch in batches:
+                inp = torch.from_numpy(batch.transpose(0,3,1,2)).float().to(device) / 255
+                # preds = model(inp)
+                # classids.append(torch.argmax(preds,1).cpu().numpy())
+                classids.append(np.random.choice([0,1], inp.shape[0]))
+            classids = np.concatenate(classids)
+        else:
+            classids = np.array([], dtype=int)
 
-            # ---- get results
-            # classids.append(torch.argmax( model.img2class(batch_arr_gpu),dim=1).detach().cpu().numpy())
-            classids.append(np.random.choice([0, 1], batch_arr_gpu.shape[0]))
-        classids = np.hstack(classids)
+        # Assign classifications
+        for (idx_arr,), cid in zip(id_out, classids):
+            allobjects[idx_arr]['properties']['classification'] = {
+                'name': classnames[cid],
+                'colorRGB': colors[cid]
+            }
 
-        for id, classid in zip(id_out, classids):
-            allobjects[int(id)]["properties"]['classification'] = {'name': classnames[classid],
-                                                                   'colorRGB': colors[classid]}
-
-# +
-# # for debugging
-# for i,(c,a) in enumerate(zip(classids,arr_out)):
-#     plt.imshow(a/255)
-#     plt.show()
-#     print(c)
-#     if i > 10:
-#         break
-
-# +
-# # for timing
-# # %load_ext line_profiler
-# # %lprun -f makeoutput makeoutput()
-# makeoutput()
-# -
-
-if json_annotated_fname.endswith(".gz"):
-    with gzip.open(json_annotated_fname, 'wt', encoding="ascii") as zipfile:
-        geojson.dump(allobjects, zipfile)
+# Save output
+if json_annotated_fname.endswith('.gz'):
+    with gzip.open(json_annotated_fname, 'wt', encoding='ascii') as out:
+        geojson.dump(allobjects, out)
 else:
-    with open(json_annotated_fname, 'w') as outfile:
-        geojson.dump(allobjects, outfile)
-
-# +
-
-#         "name": "Positive", # add incolors
-#         "colorRGB": -377282
-
-#         "name": "Other",
-#         "colorRGB": -14336
-
-#         "name": "Stroma",
-#         "colorRGB": -6895466
-
-
-#         "name": "Necrosis",
-#         "colorRGB": -13487566
-
-#         "name": "Tumor",
-#         "colorRGB": -3670016
-
-#         "name": "Immune cells",
-#         "colorRGB": -6268256
-
-
-#         "name": "Negative",
-#         "colorRGB":-9408287
-
-
-# +
-# This code to perform entire polygon with complex objects
-# exteriors = [int_coords(geo.coords) for hit in hits for geo in hit.boundary.geoms ] #need this modificatrion for complex structures
-
-# This code to perform by center with complex objects
-# exteriors = [int_coords(geo.coords) for hit in hits for geo in allshapes[hit.id].boundary.geoms ]
+    with open(json_annotated_fname, 'w') as out:
+        geojson.dump(allobjects, out)
--- CreatePlot.py
+++ CreatePlot.py
@@ -1,22 +1,63 @@
+#!/usr/bin/env python3
+import os
+import gzip
+import geojson
+import numpy as np
+import pandas as pd
+import openslide
+from shapely.geometry import shape, Polygon
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import pandas as pd
-exercise = pd.read_csv("exercise.csv")
+# ─── Configuration ─────────────────────────────────────────────────────────────
+openslidelevel       = 0
+tilesize             = 10000        # must match your analysis script
+gsi_fname            = '1L1_nuclei_reg_anno.json'
+wsi_fname            = 'MTA3-8_GPC3.svs'
 
-# Set the figure size and style
-plt.figure(figsize=(10, 6))
+# ─── Load annotated GeoJSON ────────────────────────────────────────────────────
+if gsi_fname.endswith('.gz'):
+    with gzip.open(gsi_fname, 'rt', encoding='ascii') as f:
+        allobjects = geojson.load(f)
+else:
+    with open(gsi_fname) as f:
+        allobjects = geojson.load(f)
+
+# ─── Extract centroids & labels ────────────────────────────────────────────────
+geoms     = [shape(obj.get('nucleusGeometry', obj.get('geometry'))) for obj in allobjects]
+centroids = [g.centroid for g in geoms]
+labels    = [obj['properties']['classification']['name'] for obj in allobjects]
+
+# ─── Open the whole-slide to get dimensions ────────────────────────────────────
+osh         = openslide.OpenSlide(wsi_fname)
+level_dims  = osh.level_dimensions[openslidelevel]
+scalefactor = int(osh.level_downsamples[openslidelevel])
+
+# ─── Tile the slide & count per class ─────────────────────────────────────────
+records = []
+for y in range(0, level_dims[1], int(tilesize * scalefactor)):
+    for x in range(0, level_dims[0], int(tilesize * scalefactor)):
+        tile_poly = Polygon([
+            (x, y),
+            (x + tilesize * scalefactor, y),
+            (x + tilesize * scalefactor, y + tilesize * scalefactor),
+            (x, y + tilesize * scalefactor),
+        ])
+        # correct list comprehension: filter centroids inside
+        hits = [i for i, c in enumerate(centroids) if tile_poly.contains(c)]
+        if not hits:
+            continue
+        for cls in set(labels):
+            count = sum(1 for i in hits if labels[i] == cls)
+            records.append({'Class': cls, 'Count': count})
+
+# ─── Build DataFrame & plot ─────────────────────────────────────────────────
+df = pd.DataFrame(records)
+
 sns.set(style="whitegrid")
-
-# Create the violin plot with overlayed swarm plot
-sns.violinplot(x="kind", y="pulse", data=exercise, palette="Set3", inner=None)
-sns.swarmplot(x="kind", y="pulse", data=exercise, color="black", size=3)
-
-# Add title and labels
-plt.title("Pulse Distribution by Type of Exercise", fontsize=16)
-plt.xlabel("Type of Exercise", fontsize=12)
-plt.ylabel("Pulse Rate", fontsize=12)
-
-# Improve layout
+plt.figure(figsize=(8, 6))
+sns.violinplot(x="Class", y="Count", hue="Class", data=df, inner="quartile", palette="Set2", legend=False)
+plt.title("Per-Tile Cell Counts by Class")
+plt.ylabel("Cells per Tile")
 plt.tight_layout()
 plt.show()
